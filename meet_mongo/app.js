ca8364a24df4c40e725580f001f863df8f7ef877
Nguyen Manh Cuong <cuong.ngman@gmail.com>
2017-01-10 23:18:17 +0700

    changed

--- meet_mongo/app.js
+++ meet_mongo/app.js
@@ -1,15 +1,25 @@
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
+const MongoClient = require('mongodb').MongoClient,
+      assert = require('assert');
+const url = "mongodb://localhost:27017/myproject"    
 
-// Connection URL
-var url = 'mongodb://localhost:27017/myproject';
+let insertDocument = function (db,callback) {
+  let collection = db.collection('documents');
+  collection.insertMany([
+    {a:1},{a:2},{a:3}
+  ],function (err,result) {
+    assert.equal(null,err);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log(`Inserted 3 documents into the collection`);
+    callback(result);
+  });
+}
 
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  db.close();
+MongoClient.connect(url,function MongoCallback(err,db) {
+  assert.equal(null,err);
+  console.log(`Connect successfully`);
+  insertDocument(db,function (result){
+    console.log(`${result.ops[0].toString()}`);
+    db.close();
+  });
 });
-console.log(`hello`);
-console.log(`Changed`);
