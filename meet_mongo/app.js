14d8460fadac817c3bea66c589dd447d591f36b2
Nguyen Manh Cuong <cuong.ngman@gmail.com>
2017-01-12 12:49:19 +0700

    Changed

--- meet_mongo/app.js
+++ meet_mongo/app.js
@@ -1,7 +1,6 @@
 const MongoClient = require('mongodb').MongoClient,
       assert = require('assert');
-const url = "mongodb://localhost:27017/myproject"    
-
+const url = "mongodb://localhost:27017/myproject";
 let insertDocument = function (db,callback) {
   let collection = db.collection('documents');
   collection.insertMany([
@@ -14,12 +13,52 @@ let insertDocument = function (db,callback) {
     callback(result);
   });
 }
+let findDocuments = function (db,ops,callback) {
+  let collection = db.collection('documents');
+  switch (ops){
+    case 'all':
+      collection.find({}).toArray(function (err,docs) {
+        assert.equal(null,err);
+        console.log(`Found the following record`);
+        console.log(docs);
+        callback(docs);
+      });
+      break;
+    default:
+      collection.find(ops).toArray(function (err,docs) {
+        assert.equal(null,err);
+        console.log(`Found the following record`);
+        console.log(docs);
+        callback(docs);
+      });
+      break;
+  }
+}
+let updateDocuments = function (db,callback) {
+  let collection = db.collection('documents');
+  collection.updateOne(
+    {a:2}, 
+    {$set: {b:1}},
+    function (err, result) {
+      assert.equal(null,err);
+      assert.equal(1,result.result.n);
+      console.log(`Updated the document with the field a equal to 2`);
+      callback(result);
+    });
+}
 
 MongoClient.connect(url,function MongoCallback(err,db) {
   assert.equal(null,err);
   console.log(`Connect successfully`);
-  insertDocument(db,function (result){
-    console.log(`${result.ops[0].toString()}`);
-    db.close();
+  insertDocument(db,function (){
+    // findDocuments(db, {a:3}, function () {
+    //   db.close();  
+    // });
+      // updateDocuments(db,function (result) {
+      //   console.log(`${result.result}`);
+      // });
+    findDocuments(db,'all',function () {
+      db.close();
+    });
   });
 });
