8ed3801ebca08443c61e95f86d3e05f4819aed09
Nguyen Manh Cuong <cuong.ngman@gmail.com>
2017-01-15 22:42:26 +0700

    Changed

--- meet_mongo/app.js
+++ meet_mongo/app.js
@@ -4,7 +4,7 @@ const url = "mongodb://localhost:27017/myproject";
 let insertDocument = function (db,callback) {
   let collection = db.collection('documents');
   collection.insertMany([
-    {a:1},{a:2},{a:3}
+    {a:1},{a:2},{a:4}
   ],function (err,result) {
     assert.equal(null,err);
     assert.equal(3, result.result.n);
@@ -46,18 +46,52 @@ let updateDocuments = function (db,callback) {
       callback(result);
     });
 }
+let removeDocuments = function (db, val, callback) {
+  let collection = db.collection('documents');
+  if(typeof(val) === 'object'){
+    collection.deleteOne(val,function (err, result) {
+    assert.equal(null,err);
+    assert.equal(1,result.result.n);
+    console.log(`Removed the document with the field equal to ${val.a}`);
+    callback(result);
+    });
+  }
+}
+let indexCollection = function (db,callback) {
+  db.collection('documents').createIndex(
+    {"a":1},
+    null,
+    function (err,results) {
+      console.log(`${results}`);
+      callback();
+    }
+  );
+
+}
 
 MongoClient.connect(url,function MongoCallback(err,db) {
   assert.equal(null,err);
   console.log(`Connect successfully`);
-  insertDocument(db,function (){
-    // findDocuments(db, {a:3}, function () {
-    //   db.close();  
-    // });
-      // updateDocuments(db,function (result) {
-      //   console.log(`${result.result}`);
-      // });
-    findDocuments(db,'all',function () {
+  // insertDocument(db,function (){
+  //   // findDocuments(db, {a:3}, function () {
+  //   //   db.close();  
+  //   // });
+  //     // updateDocuments(db,function (result) {
+  //     //   console.log(`${result.result}`);
+  //     // });
+  //   // findDocuments(db,'all',function () {
+  //   //   db.close();
+  //   // });
+  // });
+
+  // removeDocuments(db, {a:4} ,function (res) {
+  //   findDocuments(db,{},function (res) {
+  //     db.close();  
+  //   });
+  // });
+
+  insertDocument(db, function (insertResult) {
+    indexCollection(db, function (indexResult) {
       db.close();
     });
   });
